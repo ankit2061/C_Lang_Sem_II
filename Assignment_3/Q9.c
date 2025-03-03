651815fc3fd2932ec7b02a96573b7b8b00930ce9
ankit-2061 <codingjourney18@gmail.com>
2025-03-03 07:41:51 +0530

    changes

--- Assignment_3/Q9.c
+++ Assignment_3/Q9.c
@@ -3,13 +3,13 @@
 #include <math.h>
 
 int main(){
-    int x,y,i,x1,e=1;
+    int x,y,i,e=1;
     // float e=1;
     printf("Enter the value of X and Y to evaulate X^Y:\n");
     scanf("%d %d",&x,&y);
-    x1=x;
     for(i=1;i<=y;i++){
         e=e*x;
     }
-    printf("%d^%d=%d ",x1,y,e);
+    printf("%d^%d=%d ",x,y,e);
+    return 0;
 }
