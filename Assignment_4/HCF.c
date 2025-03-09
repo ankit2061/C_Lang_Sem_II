180a48c5e0ad23f9c8914d3840ebf73bf34c1eae
ankit-2061 <codingjourney18@gmail.com>
2025-03-09 11:23:42 +0530

    Changes in HCF

--- Assignment_4/HCF.c
+++ Assignment_4/HCF.c
@@ -1,16 +1,20 @@
 // Find HCF of two numbers (by atleast two methods).
 #include <stdio.h>
 int main(){
-    int a,b,r;
-    printf("Enter two numbers: ");
-    scanf("%d %d",a,b);
-    r=((a<b)?a:b);//Smallest of the two numbers
+    int a,b,r=0;
+    printf("Enter two numbers: \n");
+    scanf("%d %d",&a,&b);
+    if(a<b)
+        r=a;
+    else
+    r=b;
+    
     while(r>0){
         if(a%r==0 &&b%r==0){
             break;
         }
         r--;
     }
-    printf("HCF: %d",r);
+    printf("HCF: %d\n",r);
     return 0;
 }
